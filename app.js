dc17163b3c587d6fd5f1ab2b01fed62e5a6d0401
Acoleman37 <aleccoleman123@yahoo.com>
2021-12-11 12:56:20 -0800

    Generate HTML File

---
+++ app.js
@@ -0,0 +1,16 @@
+const generatePage = require('./Src/page-template');
+
+const fs = require('fs');
+
+// Captures user input data
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log ('Portfolio complete! Checkout index.html to see the output!');
+});
+
+
